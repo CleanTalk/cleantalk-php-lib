b3bfad264c3333253babe66c01e6b0e3796bcd2f
safronik <Githubishe351@$^>
2021-03-19 15:45:53 +0500

    Mod: \Cleantalk\Common\Templates\Singleton updated to 1.1.0.  Variable number of parameters added for PHP lower 5.6.

--- Common/Templates/Singleton.php
+++ Common/Templates/Singleton.php
@@ -2,31 +2,49 @@
 
 namespace Cleantalk\Common\Templates;
 
+/**
+ * Trait Singleton
+ *
+ * @package CleanTalk
+ * @Version 1.1.0
+ * @author Cleantalk team (welcome@cleantalk.org)
+ * @copyright (C) 2014 CleanTalk team (http://cleantalk.org)
+ * @license GNU/GPL: http://www.gnu.org/copyleft/gpl.html
+ */
+
 trait Singleton{
-	
-	static $instance;
-	
-	public function __construct(){}
-	public function __wakeup(){}
-	public function __clone(){}
-	
-	/**
-	 * Constructor
-	 * @return $this
-	 */
-	public static function getInstance(){
-		if (!isset(static::$instance)) {
-			static::$instance = new static;
-			static::$instance->init();
-		}
-		return static::$instance;
-	}
-	
-	/**
-	 * Alternative constructor
-	 */
-	private function init(){
-	
-	}
-	
+    
+    public static $instance;
+    
+    public function __construct(){}
+    public function __wakeup(){}
+    public function __clone(){}
+    
+    /**
+     * Constructor
+     * @return $this
+     */
+    public static function getInstance(){
+        
+        $args = func_get_args();
+        
+        if( ! isset( static::$instance ) || ! $args){
+            static::$instance = new static();
+            if( ! empty( $args) )
+                static::$instance->init( $args );
+            else
+                static::$instance->init();
+        }
+        return static::$instance;
+    }
+    
+    /**
+     * Alternative constructor
+     *
+     * @param null $args
+     */
+    protected function init( $args = null ){
+    
+    }
+    
 }
