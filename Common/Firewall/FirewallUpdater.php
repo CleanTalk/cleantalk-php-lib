a1409701f10d050029d5b785d14759818b69a3c0
Artem Davydov <yozzeebeatz@gmail.com>
2021-04-20 15:13:21 +0500

    Fix: table prefix in fw.

--- Common/Firewall/FirewallUpdater.php
+++ Common/Firewall/FirewallUpdater.php
@@ -54,7 +54,7 @@ class FirewallUpdater
     {
         $this->api_key            = $api_key;
         $this->db                 = $db;
-        $this->fw_data_table_name = $fw_data_table_name;
+        $this->fw_data_table_name = $db->prefix . $fw_data_table_name;
         $this->helper             = new Helper();
         $this->api                = new API();
     }
@@ -359,8 +359,8 @@ class FirewallUpdater
             $sql = sprintf( Schema::getSchema('sfw'), $this->db->prefix );
             $this->db->execute( $sql );
         }
-        $this->db->execute( 'CREATE TABLE IF NOT EXISTS `' . APBCT_TBL_FIREWALL_DATA . '_temp` LIKE `' . APBCT_TBL_FIREWALL_DATA . '`;' );
-        $this->db->execute( 'TRUNCATE TABLE `' . APBCT_TBL_FIREWALL_DATA . '_temp`;' );
+        $this->db->execute( 'CREATE TABLE IF NOT EXISTS `' . $this->fw_data_table_name . '_temp` LIKE `' . $this->fw_data_table_name . '`;' );
+        $this->db->execute( 'TRUNCATE TABLE `' . $this->fw_data_table_name . '_temp`;' );
     }
 
     /**
@@ -370,7 +370,7 @@ class FirewallUpdater
      */
     private function deleteMainDataTables()
     {
-        $this->db->execute( 'DROP TABLE `'. APBCT_TBL_FIREWALL_DATA .'`;' );
+        $this->db->execute( 'DROP TABLE `'. $this->fw_data_table_name .'`;' );
     }
 
     /**
@@ -380,7 +380,7 @@ class FirewallUpdater
      */
     private function renameDataTables()
     {
-        $this->db->execute( 'ALTER TABLE `'. APBCT_TBL_FIREWALL_DATA .'_temp` RENAME `'. APBCT_TBL_FIREWALL_DATA .'`;' );
+        $this->db->execute( 'ALTER TABLE `'. $this->fw_data_table_name .'_temp` RENAME `'. $this->fw_data_table_name .'`;' );
     }
 
 }
