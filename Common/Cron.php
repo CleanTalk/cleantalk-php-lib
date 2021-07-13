67a130b51df57217d0827c2293232a1f69eb743f
Glomberg <bazz@bk.ru>
2021-07-13 11:33:22 +0300

    Fix. Cron. Cron class fixed.

--- Common/Cron.php
+++ Common/Cron.php
@@ -101,20 +101,22 @@ abstract class Cron
     {
         // First call time() + period
         $first_call = ! $first_call ? time() + $period : $first_call;
-        
-        if( isset( $this->tasks[ $task ] ) ){
+
+        $tasks = ! empty( $this->tasks ) ? $this->tasks : $this->getTasks();
+
+        if( isset( $tasks[ $task ] ) ){
             return false;
         }
         
         // Task entry
-        $this->tasks[$task] = array(
+        $tasks[$task] = array(
             'handler'   => $handler,
             'next_call' => $first_call,
             'period'    => $period,
             'params'    => $params,
         );
-        
-        return $this->saveTasks( $this->tasks );
+
+        return $this->saveTasks( $tasks );
     }
     
     /**
@@ -126,13 +128,14 @@ abstract class Cron
      */
     public function removeTask( $task )
     {
-        if( ! isset( $this->tasks[ $task ] ) ){
+        $tasks = ! empty( $this->tasks ) ? $this->tasks : $this->getTasks();
+        if( ! isset( $tasks[ $task ] ) ){
             return false;
         }
-        
-        unset( $this->tasks[ $task ] );
 
-        return $this->saveTasks( $this->tasks );
+        unset( $tasks[ $task ] );
+
+        return $this->saveTasks( $tasks );
     }
 
     /**
@@ -153,7 +156,7 @@ abstract class Cron
             // Rewrite the task
             $tasks[$task] = array(
                 'handler'   => $handler,
-                'next_call' => $first_call,
+                'next_call' => is_null( $first_call ) ? time() + $period : $first_call,
                 'period'    => $period,
                 'params'    => $params,
             );
